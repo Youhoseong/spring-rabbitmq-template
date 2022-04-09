c255a19ecf009b15dc49cb9d75628a575ea56202
Youhoseong <h0song@naver.com>
2022-04-10 01:35:57 +0900

    synchronous queue push api #1

--- src/main/java/kep/winter/amqprabbit/CafeMessageListener.java
+++ src/main/java/kep/winter/amqprabbit/CafeMessageListener.java
@@ -4,14 +4,24 @@ package kep.winter.amqprabbit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class CafeMessageListener {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @RabbitListener(queues = "coffee.queue")
-    public void receiveMessage(final Message message) {
+    public String receiveMessage(final Message message) {
         log.info("message={}", new String(message.getBody()));
+        return "reply";
     }
+
+
+
 }
